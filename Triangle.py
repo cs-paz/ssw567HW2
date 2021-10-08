4f747749187acbfdb1a97df304e59cab1a3d441c
Christian Szablewski-Paz <cszablewskipaz@gmail.com>
2021-10-08 19:52:27 -0400

    commit changes

--- Triangle.py
+++ Triangle.py
@@ -2,31 +2,11 @@
 """
 Created on Thu Jan 14 13:44:00 2016
 Updated Jan 21, 2018
-
-The primary goal of this file is to demonstrate a simple python program to classify triangles
-
 @author: jrr
 @author: rk
 """
 
 def classifyTriangle(a,b,c):
-    """
-    Your correct code goes here...  Fix the faulty logic below until the code passes all of 
-    you test cases. 
-    
-    This function returns a string with the type of triangle from three integer values
-    corresponding to the lengths of the three sides of the Triangle.
-    
-    return:
-        If all three sides are equal, return 'Equilateral'
-        If exactly one pair of sides are equal, return 'Isoceles'
-        If no pair of  sides are equal, return 'Scalene'
-        If not a valid triangle, then return 'NotATriangle'
-        If the sum of any two sides equals the squate of the third side, then return 'Right'
-      
-      BEWARE: there may be a bug or two in this code
-    """
-
     # require that the input values be >= 0 and <= 200
     if a > 200 or b > 200 or c > 200:
         return 'InvalidInput'
